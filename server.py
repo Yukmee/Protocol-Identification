a6322011a5922abf979e2e18c971299cbf5d5c75
NateLee <nateleee.dev@gmail.com>
2019-09-08 19:10:13 +0800

    可以上传文件了.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html', test_var='test_var!')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f'./saved_pcaps/{secure_filename(f.filename)}')
+
+        return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
