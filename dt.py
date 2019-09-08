21ede0e43e4f67d1e2ad1a57652032bbca165869
NateLee <nateleee.dev@gmail.com>
2019-09-08 15:05:59 +0800

    Made it just for now.

--- dt.py
+++ dt.py
@@ -6,6 +6,8 @@ from sklearn import metrics
 from protocol_dict import protocol_dict
 import pandas as pd
 import numpy as np
+import pickle
+
 
 dataset = pd.read_csv("./features_all_in_one.csv")
 X = dataset.drop('label', axis=1)
@@ -19,10 +21,23 @@ classifier = DecisionTreeClassifier()
 classifier.fit(X_train, y_train)
 y_pred_a = classifier.predict(X_test)
 y_pred = regressor.predict(X_test)
-print(confusion_matrix(y_test, y_pred_a))
-print(classification_report(y_test, y_pred_a))
-df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+# print(confusion_matrix(y_test, y_pred_a))
+# print(classification_report(y_test, y_pred_a))
+# df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+# print(df)
+# print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+# print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+# print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+# Save model to disk
+pickle.dump(regressor, open('saved_model.pkl', 'wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('saved_model.pkl', 'rb'))
+model_y_pred = model.predict(X_test)
+
+# Print predictions from saved model.
+df = pd.DataFrame({'Actual': y_test, 'Predicted': model_y_pred})
 print(df)
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
