1a6c49ea9ee1a00642797c705dc176419c999348
NateLee <nateleee.dev@gmail.com>
2019-09-08 19:54:14 +0800

    添加了 Select Box.

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', test_var='test_var!')
+    return render_template('index.html', test_var='test_var!!!')
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
@@ -14,7 +14,6 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f'./saved_pcaps/{secure_filename(f.filename)}')
-
         return 'file uploaded successfully'
 
 
