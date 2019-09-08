f4c3636e0a8a99779cf96c0f7667c379ad17c3b4
NateLee <nateleee.dev@gmail.com>
2019-09-08 16:40:30 +0800

    晚饭前的commit.

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
-import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html', test_var='test_var!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
