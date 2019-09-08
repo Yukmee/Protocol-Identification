a62867fa1df23614cd1af349da02d24064e77efd
NateLee <nateleee.dev@gmail.com>
2019-09-08 20:03:34 +0800

    index.html 初步写好

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import pickle
+
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
@@ -6,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', test_var='test_var!!!')
+    return render_template('index.html', output=['a', 'b'])
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
@@ -14,7 +16,7 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f'./saved_pcaps/{secure_filename(f.filename)}')
-        return 'file uploaded successfully'
+        return render_template('index.html', test_var='文件成功上传!')
 
 
 if __name__ == '__main__':
