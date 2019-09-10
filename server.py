058d7f8e4417c12c7785dae5ef00d5c5546ab236
NateLee <nateleee.dev@gmail.com>
2019-09-10 15:37:26 +0800

    Updated.

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import pickle
+from abc import ABC
+
 import parse_pcap
 import protocol_dict
+from collections import Counter
 from flask import Flask, render_template, request
+from flask_table import Table, Col
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -9,6 +13,7 @@ app = Flask(__name__)
 uploaded_pcap_file_name = ''
 
 
+# Helper Methods
 def find_key_by_value(x):
     for key in protocol_dict.protocol_dict.keys():
         if protocol_dict.protocol_dict[key] == x:
@@ -40,13 +45,44 @@ def predict():
     # Loading model to compare the results
     model = pickle.load(open('saved_model.pkl', 'rb'))
     y_pred = model.predict(x_test)
-    # 按出现频率排序
-    y_pred.sort()
     sth = [find_key_by_value(x) for x in y_pred]
+    # 使用 Counter() 来列出元素的频率
+    sth_new = dict(Counter(sth))
+    total_session = len(y_pred)
+
+    # Declare your table
+    class ItemTable(Table):
+        proto = Col('协议')
+        possibility = Col('可能性')
+
+    # Get some objects
+    class Item(object):
+        def __init__(self, proto, possibility):
+            self.proto = proto
+            self.possibility = possibility
+
+    # items = [Item('Name1', 'Description1'),
+    #          Item('Name2', 'Description2'),
+    #          Item('Name3', 'Description3')]
+    # Or, equivalently, some dicts
+    items = [dict(proto='Name1', possibility='Description1'),
+             dict(proto='Name2', possibility='Description2'),
+             dict(proto='Name3', possibility='Description3')]
+
+    items = [Item(x, sth_new[x] / total_session) for x in sth_new]
+
+    # Populate the table
+    table = ItemTable(items)
+
+    # return render_template('index.html',
+    #                        file_name=uploaded_pcap_file_name,
+    #                        output=f'{sth_new}',
+    #                        table=table
+    #                        )
 
     return render_template('index.html',
-                           file_name=uploaded_pcap_file_name,
-                           output=f'{sth}')
+                           table=table
+                           )
 
 
 if __name__ == '__main__':
